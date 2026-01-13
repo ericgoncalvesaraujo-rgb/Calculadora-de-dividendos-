41bfad3cc8f2405d257795ff9eaa4a5bacb2488d
ericgoncalvesaraujo-rgb <ericgoncalvesaraujo@gmail.com>
2026-01-13 06:03:18 +0000

    2

--- calculadora.py
+++ calculadora.py
@@ -5,15 +5,42 @@ import yfinance as yf
 from datetime import datetime
 from decimal import Decimal
 
-#coleta de dados
+hj = datetime.now().year
+#coleta de dados da ação escolhida
+
+#pega uma ação do yahoo finance e adiciona a sigla ".SA" por ser br
+Ticket = str(input("Digite o código: ")).upper().strip()
+nome = yf.Ticker(f"{Ticket}.SA")
+df = nome.history(interval="1d", period="max")
+
+#reseta o index para que a data se torne uma coluna
+df.reset_index(inplace = True)
+ 
+#pega a informação da data do inicio dos aportes
+data = int(input("Data do começo dos aportes: "))
+
+#garante data possível 
+while data > hj:
+ if data > hj:
+  print("data impossível!!!")
+  data = int(input("Data do começo dos aportes: "))
 
 
-Ticket = str(input("Digite o codigo da ação")).strip().upper()
-Ticker = yf.Ticker(f"{Ticket}.SA")
-acao = Ticker.history(interval="1d", period="max")
+#pega o valor inicial do aporte
+valor_inicial = float(input("digite o valor do aporte: "))
+valor_inicial = round(valor_inicial, 2)
 
-print(acao)
 
+
+#organização de dados
+
+#organizando arquivos a partir da data escolhida
+df["Date"] = pd.to_datetime(df["Date"])
+df = df[df["Date"].dt.year >= data]
+df_ano_mes = df
+df_ano_mes["Date"] = pd.to_datetime(df["Date"]).dt.to_period("M")
+
+print(df_ano_mes)
 
 
 
