25a56a73b9f99c9069968c4dc8c985f777ed165e
Erick Gonçalves Araujo <ericgoncalvesaraujo@gmail.com>
2026-03-09 02:34:46 +0000

    4

--- streamlit.py
+++ streamlit.py
@@ -4,3 +4,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from streamlit_plotly_events import plotly_events
+
+st.set.page_config(page_title="Calculadora de dividendos")
+st.title("Calculadora de dividentos", color="blue")
