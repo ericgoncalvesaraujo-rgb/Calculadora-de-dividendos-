a03368af96c9c719c76eae81ad7c9e983b06f044
Erick Gonçalves Araujo <ericgoncalvesaraujo@gmail.com>
2026-03-09 03:02:16 +0000

    3

--- streamlit.py
+++ streamlit.py
@@ -9,16 +9,16 @@ st.set_page_config(page_title="Calculadora de dividendos")
 st.title("Calculadora de dividentos")
 
 #coleta de dados da ação escolhida
-while True:
- #pega uma ação do yahoo finance e adiciona a sigla ".SA" por ser br
- Ticket = str(st.text_input("Digite o código: ")).upper().strip()
- nome = yf.Ticker(f"{Ticket}.SA")
- df = nome.history(interval="1d", period="max")
- #garante que passe somente a ação correta
- if not df.empty:
-  break
- else:
-  st.error("acao incorreta ou não existente")
+
+#pega uma ação do yahoo finance e adiciona a sigla ".SA" por ser br
+Ticket = str(st.text_input("Digite o código: ")).upper().strip()
+nome = yf.Ticker(f"{Ticket}.SA")
+df = nome.history(interval="1d", period="max")
+#garante que passe somente a ação correta
+if not df.empty:
+   st.sucess("Ação encontrada!!!")
+else:
+   st.error("acao incorreta ou não existente")
 
 #reseta o index para que a data se torne uma coluna
 df.reset_index(inplace=True)
@@ -26,20 +26,19 @@ df.reset_index(inplace=True)
 #pegando as datas e garantindo serem possíveis
 
 hj = dt.datetime.now().year
-while True:
- data = int(st.number_input("Ano do começo dos aportes: "))
- if data <= hj:
-  break
- else:
+
+data = st.number_input("Ano do começo dos aportes: ", min_value=1900, max_value=hj, step=1)
+if data <= hj:
+  st.sucess("Data aceita!!!")
+else:
   st.error("data impossivel!!!")
 
 #pega o valor inicial do aporte
-while True:
- valor_inicial = st.number_input("Digite o valor do primeiro aporte: ")
- valor_inicial = round(valor_inicial, 2)
- if valor_inicial >= df['Close'].iloc[0]:
-  break
- else:
+valor_inicial = st.number_input("Digite o valor do primeiro aporte: ")
+valor_inicial = round(valor_inicial, 2)
+if valor_inicial >= df['Close'].iloc[0]:
+  st.sucess("Valor do aporte aceito!!!")
+else:
   st.error("Seu aporte é insuficiente para comprar a ação nesse ano")
 
 #pegar valor do aporte mensal
