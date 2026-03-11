65e519d118d1363e99730e4e6d55bc7fabb09574
Erick Gonçalves Araujo <ericgoncalvesaraujo@gmail.com>
2026-03-11 10:53:36 +0000

    6

--- streamlit.py
+++ streamlit.py
@@ -42,7 +42,7 @@ st.markdown("""
 """)
 
 #caixa de ações
-if "açoes" not in st.session_state:
+if "acoes" not in st.session_state:
  st.session_state.acoes = []
 
 #criando um dataframe vazio para receber os dados da ação
@@ -50,10 +50,21 @@ df = pd.DataFrame()
 
 #pega uma ação do yahoo finance e adiciona a sigla ".SA" por ser br
 if not st.session_state.acoes:
-
+ ticket = str(st.text_input("Digite o código: ")).upper().strip()
+ if ticket not in st.session_state.acoes:
+   st.session_state.acoes.append(ticket)
+   acao = yf.Ticker(f"{ticket}.SA")
+   df = acao.history(interval="1d", period="max")
+  
+   if df.empty:
+      st.error("acao incorreta ou não existente")
+#reseta o index para que a data se torne uma coluna
+   else:
+        df.reset_index(inplace=True)
+        st.info("acao encontrada!!!")
+ if not df.empty:
   with st.form("formulario de acoes"):
-   ticket = str(st.text_input("Digite o código: ")).upper().strip()
-
+  
    hj = dt.datetime.now().year
    data = st.number_input("Ano do começo dos aportes: ", min_value=1, max_value=hj, step=1)
 
@@ -61,55 +72,42 @@ if not st.session_state.acoes:
 
    aporte_mensal = st.number_input("Digite o valor do aporte mensal (caso não for usar coloque 0): ", min_value=0, step=10)
     
-   botao_questionario = dt.submit_button("Enviar")
+   botao_questionario = st.submit_button("Enviar")
 #garante que passe somente a ação correta
-  if botao_questionario:
-    if ticket not in st.session_state:
-      st.session_state.acoes.append(ticket)
-    acao = yf.Ticker(f"{ticket}.SA")
-    df = acao.history(interval="1d", period="max")
-  
-    if not df.empty:
-        st.success("Ação encontrada!!!")
-#reseta o index para que a data se torne uma coluna
-        df.reset_index(inplace=True)
-    else:
-        st.error("acao incorreta ou não existente")
-
-
+   if botao_questionario:
 #pegando as datas e garantindo serem possíveis
-  if data:
-    if data <= hj:
-            st.success("Data aceita!!!")
-    else:
-            st.error("data impossivel!!!")
+    if data:
+      if data <= hj:
+              st.success("Data aceita!!!")
+      else:
+              st.error("data impossivel!!!")
+        
+  #pega o valor inicial do aporte
       
-#pega o valor inicial do aporte
+    if valor_inicial:
+            valor_inicial = round(valor_inicial, 2)
+      
+    if valor_inicial >= df['Close'].iloc[0]:
+              st.success("Valor do aporte aceito!!!")
+    else:
+              st.error("Seu aporte é insuficiente para comprar a ação nesse ano")
+
+  #pegar valor do aporte mensal
     
-  if valor_inicial:
-          valor_inicial = round(valor_inicial, 2)
+    if aporte_mensal:
+      aporte_mensal = round(aporte_mensal, 2)
+      st.info("Valor do aporte mensal aceito!!!")
+      if aporte_mensal == 0:
+        st.info('Não tera aportes mensais')
+      
+
+  #organizando arquivos a partir da data 
     
-  if valor_inicial >= df['Close'].iloc[0]:
-            st.success("Valor do aporte aceito!!!")
-  else:
-            st.error("Seu aporte é insuficiente para comprar a ação nesse ano")
+    df[df["Date"]] = pd.to_datetime(df["Date"])
+    df = df[df["Date"].dt.datetime.year >= data]
 
-#pegar valor do aporte mensal
-   
-  if aporte_mensal:
-    aporte_mensal = round(aporte_mensal, 2)
-    st.info("Valor do aporte mensal aceito!!!")
-    if aporte_mensal == 0:
-      st.info('Não tera aportes mensais')
+
+
     
-
-#organizando arquivos a partir da data 
-  
-  df[df["Date"]] = pd.to_datetime(df["Date"])
-  df = df[df["Date"].dt.datetime.year >= data]
-
-
-
-  
-  
+    
 
