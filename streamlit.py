859f7a4dcdfc9fb09da160c635f4ff616cf3d873
Erick Gonçalves Araujo <ericgoncalvesaraujo@gmail.com>
2026-03-12 00:41:35 +0000

    7

--- streamlit.py
+++ streamlit.py
@@ -80,7 +80,7 @@ if ticket:
 
            aporte_mensal = st.number_input("Digite o valor do aporte mensal (caso não for usar coloque 0): ", min_value=0, step=10)
 
-           botao_formulario = st.form_submit_button("Confirmar")
+           
          #pegando as datas e garantindo serem possíveis    
           if data:
             if data <= hj:
@@ -102,11 +102,12 @@ if ticket:
               st.success("Aportes mensais aceitos!!!")
               if aporte_mensal == 0:
                st.info('Não tera aportes mensais')
-            
+
+          botao_formulario = st.form_submit_button("Confirmar") 
 
 
           #organizando arquivos a partir da data 
-            if botao_formulario:
+          if botao_formulario:
                   st.session_state.df["Date"] = pd.to_datetime(st.session_state.df["Date"])
                   df = st.session_state.df[st.session_state.df["Date"].dt.year >= data]
                   st.info("Em fase de testes")
