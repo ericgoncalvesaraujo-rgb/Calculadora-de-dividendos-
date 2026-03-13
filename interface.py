ea51d41de731edd84ac6e1c37429a527778910eb
Erick Gonçalves <ericgoncalvesaraujo@gmail.com>
2026-03-13 14:50:09 -0300

    9

--- interface.py
+++ interface.py
@@ -21,10 +21,10 @@ label, p, h1, h2, h3, h4 { color: #0E1117; }
 
 st.title("📊 Calculadora de Dividendos")
 
-#organizando
+#organizando as variáveis de sessão
 
 if "calcular" not in st.session_state:
-  st.session_state.calcular = False                      # variavel para controlar o fluxo do código
+  st.session_state.calcular = False                      
 
 if "acoes" not in st.session_state:
     st.session_state.acoes = []
@@ -90,11 +90,64 @@ if st.session_state.calcular:
  df_ano_mes = df_ano_mes.groupby("Date").agg({
     "Dividends" : "sum", "Close" : "last"
  })
+ df_ano_mes. reset_index(inplace=True)
 
- fig  =  px.line(df_ano_mes, x=df_ano_mes.index.astype(str), y=(["Dividends", "Close"]), title="Evoloção reinvestindo ou não os dividendos")
+ saldo = valor_inicial % df_ano_mes["Date"].iloc[0]
+ saldo_sem_divi = valor_inicial % df_ano_mes["Date"].iloc[0]
+
+ quanti_acao = valor_inicial // df_ano_mes["Date"].iloc[0]
+ 
+ aporte_somado = 0
+
+ divi_somado = 0
+
+ df_final = []  
+
+ for row in df_ano_mes.itertuples():
+  saldo += (row.Dividends * quanti_acao ) + aporte_mensal
+  acao_comprada = saldo // row.Close
+  quanti_acao += acao_comprada
+  saldo = saldo % row.Close
+  patrimonio = quanti_acao * row.Close + saldo
+  dividendo_recebido = row.Dividends * quanti_acao
 
 
+
+  saldo_sem_divi += aporte_mensal
+  acao_comprada_sem_divi = saldo_sem_divi // row.Close
+  quanti_acao_sem_divi += acao_comprada_sem_divi
+  saldo_sem_divi = saldo_sem_divi % row.Close
+  patrimonio_sem_divi = quanti_acao_sem_divi * row.Close + saldo_sem_divi
+  dividendos_recebidos_sem_divi = row.Dividends * quanti_acao_sem_divi
+   
+  aporte_somado += aporte_mensal
+  divi_somado += row.Dividends * quanti_acao
+  
+  
+  
+  df_final.append({
+    "Data" : row.Date,
+    "Saldo" : saldo,
+    "Saldo_sem_reinvestir" : saldo_sem_divi,
+    "Preço da ação" : row.Close,
+    "Ações_compradas" : acao_comprada,
+    "Ações_compradas_sem_reinvestir" : acao_comprada_sem_divi,
+    "Dividendos_recebidos" : dividendo_recebido,
+    "Dividendos_recebidos_sem_reinvestir" : dividendos_recebidos_sem_divi,
+    "Total de ações" : quanti_acao,
+    "Total de ações sem reinvestir" : quanti_acao_sem_divi,
+    "Patrimônio" : patrimonio,
+    "Patrimônio sem reinvestir" : patrimonio_sem_divi
+  })
+ 
+ df_final = pd.DataFrame(df_final)
+ 
+ 
+ fig  =  px.line(df_ano_mes, x=df_final["Date"], y=(["Patrimônio", "Patrimônio sem reinvestir"]), title="Evoloção reinvestindo ou não os dividendos")
+ 
+ 
  st.plotly_chart(fig, use_container_width=True)
+ st.bar_chart(df_final[["Data", "Dividendos_recebidos", "Dividendos_recebidos_sem_reinvestir"]].set_index("Data"))
 
 
 
