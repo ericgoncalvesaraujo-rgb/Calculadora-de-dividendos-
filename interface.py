a2ee034ce46073f4c9d94e29c61ab86d4370b933
Erick Gonçalves <ericgoncalvesaraujo@gmail.com>
2026-03-13 15:55:04 -0300

    bom

--- interface.py
+++ interface.py
@@ -165,8 +165,8 @@ if st.session_state.calcular:
   st.plotly_chart(fig2, use_container_width=True)
   st.plotly_chart(fig3, use_container_width=True)
  
- st.write(f"Se não reinvestisse os dividendos, seu patrimonio final seria de R$: {df_final["Patrimônio sem reinvestir"].iloc[-1]:,.2f} com um lucro de {lucro_sem_reinvestir}\n\n")
- st.write(f"Reinvestindo os dividendos, seu patrimonio final seria de R$: {df_final["Patrimônio"].iloc[-1]:,.2f} com um lucro de {lucro_reinvestindo}")
+ st.write(f"Se não reinvestisse os dividendos, seu patrimônio final seria de R$: {df_final['Patrimônio sem reinvestir'].iloc[-1]:,.2f} com um lucro de {lucro_sem_reinvestir}\n")
+ st.write(f"Reinvestindo os dividendos, seu patrimônio final seria de R$: {df_final['Patrimônio'].iloc[-1]:,.2f} com um lucro de {lucro_reinvestindo}")
 
 
 
