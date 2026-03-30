1690eb963ea2139c6758e067820c381eced2ec7c
Erick Gonçalves <ericgoncalvesaraujo@gmail.com>
2026-03-30 15:55:42 -0300

    4

--- interface.py
+++ interface.py
@@ -13,7 +13,7 @@ st.markdown("""
 <style>
 .stApp { background-color: white !important ; color: #0E1117  ; }
             
-button { background-color: #grey !important;color: #white !important; }
+button { background-color: #grey !important;color: #0E1117 !important; }
                       
             
 label, p, h1, h2, h3, h4 { color: white! important; }
