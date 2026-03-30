167debc7faa3267a55cffe4db8ee93f95695087f
Erick Gonçalves <ericgoncalvesaraujo@gmail.com>
2026-03-30 15:47:05 -0300

    4

--- interface.py
+++ interface.py
@@ -11,9 +11,9 @@ st.set_page_config(page_title="Calculadora de Dividendos")
 #markdown
 st.markdown("""
 <style>
-.stApp { background-color: white; color: #grey; }
+.stApp { background-color: white; color: #0E1117 ; }
             
-button { background-color: #grey !important;color: white !important; }
+button { background-color: #grey !important;color: #0E1117 !important; }
                        
 label, p, h1, h2, h3, h4 { color: #0E1117; }
             
